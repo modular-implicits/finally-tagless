2daf4f6195f40f041bbae5d97bc61123aa8bde7d
Daniel Vlasits <dvlasits@gmail.com>
2023-08-17 17:28:32 +0100

    adding better test

--- tests/test.ml
+++ tests/test.ml
@@ -6,6 +6,18 @@ let () = print_endline "Hello World";;
 (* Slightly takes away from the beauty that you have to state the 
     type of the representation *)
 
+(* This is very nice and works well *)
+(* same data two different computations! *)
+  
+let () = 
+  begin
+    let x {M : Arithmetic} = M.add (M.num 5) (M.num 10) in
+    let y = x {EvalInterpreter} in
+    assert (15 = y.unEval);
+    let z = x {PrettyPrintInterpreter} in
+    assert ("(5 + 10)" = z.unPrettyPrint);
+end;;
+
 let () = 
   begin 
     let x = ((num 5) : int eval) in 
@@ -13,4 +25,7 @@ let () =
     assert (15 = y.unEval); 
     let z = add ((num 10) : int pretty_print) (num 5) in
     assert ("(10 + 5)" = z.unPrettyPrint);
-end
+  end;;
+
+
+
