f961f80c94486c7842a6efd29b76bba0821a3f01
danielvlasits <dvlasits@gmail.com>
2023-08-17 12:30:11 +0100

    adding tests

--- tests/test.ml
+++ tests/test.ml
@@ -5,6 +5,7 @@ let () = print_endline "Hello World";;
 
 let () = 
   begin 
-    let x = num 5 in 
-    print_endline (string_of_int (x.unEval)); 
+    let x = ((num 5) : int eval) in 
+    let y = add (num 10) x in
+    print_endline (string_of_int (y.unEval)); 
   end
