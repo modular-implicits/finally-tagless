78b39a57d15e812afc3db8eacd066cdf3933e6af
danielvlasits <dvlasits@gmail.com>
2023-08-17 12:22:17 +0100

    Can't handle bi-directional type inference

--- tests/test.ml
+++ tests/test.ml
@@ -1,5 +1,10 @@
-(test
- (name test)
- (modules test)
- (modes native)
- (libraries tagless))
+open Tagless.Tag;;
+
+
+let () = print_endline "Hello World";;
+
+let () = 
+  begin 
+    let x = num 5 in 
+    print_endline (string_of_int (x.unEval)); 
+  end
